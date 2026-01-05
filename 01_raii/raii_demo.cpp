ade7eb3001fb72563a801089401e3a62341f9cc6
Jorge <jorgetrigoura1@gmail.com>
2026-01-04 20:23:55 -0500

    Add buffer class

---
+++ 01_raii/raii_demo.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <cstring>
+#include <print>
+
+class Buffer {
+public:
+    explicit Buffer(size_t size)
+        : size_(size), data_(new char[size]) {
+        std::cout << "Allocated " << size_ << " bytes\n";
+    }
+
+    ~Buffer() {
+        delete[] data_;
+        std::cout << "Freed buffer\n";
+    }
+
+    Buffer(const Buffer& other)
+        : size_(other.size_), data_(new char[other.size_]) {
+        std::memcpy(data_, other.data_, size_);
+        std::cout << "Copied buffer\n";
+    }
+
+    Buffer(Buffer&& other) noexcept
+        : size_(other.size_), data_(other.data_) {
+        other.data_ = nullptr;
+        other.size_ = 0;
+        std::cout << "Moved buffer\n";
+    }
+
+private:
+    size_t size_;
+    char* data_;
+};
+
+int main() {
+    Buffer a(1024);
+    Buffer b = a;
+    Buffer c = std::move(a);
+    std::print("{}\n",sizeof(long));
+}
